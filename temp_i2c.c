e82233a4c60c1bb52a8345148d0cb9cd5b745724
sahanasadagopan <sahana.sadagopan@gmail.com>
2017-10-28 17:18:02 -0600

    just collecting temperature

--- temp_i2c.c
+++ temp_i2c.c
@@ -1,85 +1,77 @@
-/*
- *  Name        : i2c.c
- *
- *
- *  Description : I2C driver that interacts with the on board
- *                I2C bus (bus 1).
- *                Contains routines like i2c_init(),
- *                i2c_read(), i2c_write().
- *
- *  Authors     : Ashwath Gundepally, CU, ECEE
- *                Sahana Sadagopan, CU, ECEE
- */
-
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <assert.h>
+
+#include "i2c.h"
+
+#define DEV_ADDR 0X48
+int fd;
+int file;
+void
+signal_handler(int signum)
+{
+
+  assert(0 == close(fd));
+
+  exit(signum);
+
+}
 
 
-#define TMP102_I2C	0x48
-#define I2C_BUS		"/dev/i2c-1"
+int main(){
 
-int main(void) {
 
-	int file;
-	char filename[40];
-	int addr = TMP102_I2C; // The I2C address
+  char *bus = "/dev/i2c-2"; /* Pins P9_19 and P9_20 */
+  int addr = 0x48;          /* The I2C address of TMP102 */
+  uint8_t *buf = malloc(sizeof(uint8_t)*2);
+  //char buf[2] = {0};
+  int temp;
+  unsigned char MSB, LSB;
 
-	sprintf(filename, I2C_BUS);
-	if ((file = open(filename, O_RDWR)) < 0) {
-		printf("Failed to open the bus.");
-		/* ERROR HANDLING; you can check errno to see what went wrong */
-		printf("error: %s (%d)\n", strerror(errno), errno);
-		exit(1);
-	}
+  float f,c;
 
-	if (ioctl(file, I2C_SLAVE, addr) < 0) {
-		printf("Failed to acquire bus access and/or talk to slave.\n");
-		/* ERROR HANDLING; you can check errno to see what went wrong */
-		printf("error: %s (%d)\n", strerror(errno), errno);
-		exit(1);
-	}
+  i2c_init(DEV_ADDR);
+ /* Register the signal handler */
+ signal(SIGINT, signal_handler);
+ 
+ while(1)
+   {
+	if((i2c_read(buf))!=SUCCESS){
+		perror("Reading error\n");
+         }
+     // Using I2C Read
+//     if (read(file,buf,2) != 2) {
+       /* ERROR HANDLING: i2c transaction failed */
+//       perror("Failed to read from the i2c bus.\n");
 
-	write(file, 0x00, 1);
+//   } 
+	else {
 
-	usleep(500);
+       MSB = buf[0];
+       LSB = buf[1];
 
-	int errCount = 0;
-	for (;;) {
-		char buf[1] = { 0 };
+       /* Convert 12bit int using two's compliment */
+       /* Credit: http://bildr.org/2011/01/tmp102-arduino/ */
+       temp = ((MSB << 8) | LSB) >> 4;
 
-		float data;
+       c = temp*0.0625;
+       f = (1.8 * c) + 32;
 
-		// Read 2 uint8 using I2C Read
-		int k = read(file, buf, 1);
-		printf("K return %d",k);
-		if ((k != 1)) {
-			errCount++;
-			printf("error: %s (%d) %d\n", strerror(errno), errno, errCount);
-		} else {
+       printf("Temp Fahrenheit: %f Celsius: %f\n", f, c);
+     }
 
-			int temperature;
+     sleep(1);
+   }
 
-			temperature = ((buf[0]) << 8) | (buf[1]);
-			temperature >>= 4;
 
-			//The tmp102 does twos compliment but has the negative bit in the wrong spot, so test for it and correct if needed
-			if (temperature & (1 << 11))
-				temperature |= 0xF800; //Set bits 11 to 15 to 1s to get this reading into real twos compliment
 
-			printf(" temp:  %04f \t error:  %d\n", temperature * 0.0625,
-					errCount);
-
-		}
-		usleep(5);
-
-	}
-	return 0;
 }
