99d11419219cf8ccecc98c0131ce2aec7c826fac
sahanasadagopan <sahana.sadagopan@gmail.com>
2017-11-02 14:28:10 -0600

    heartbeat for all task done

--- src/main.c
+++ src/main.c
@@ -25,9 +25,13 @@ pthread_t decision_td;
 sem_t temp_sem;
 
 
-pthread_mutex_t htimelock=PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t temp_lock=PTHREAD_MUTEX_INITIALIZER;
 
 pthread_mutex_t decision_mutex=PTHREAD_MUTEX_INITIALIZER;
+
+pthread_mutex_t log_mutex=PTHREAD_MUTEX_INITIALIZER;
+
+pthread_mutex_t light_mutex=PTHREAD_MUTEX_INITIALIZER;
 
 /*Pthread Attributes*/
 pthread_attr_t temperature_attr;
@@ -41,8 +45,12 @@ struct sched_param temp_params;
 /*sigatomic*/
 static volatile sig_atomic_t count_temp=1;
 static volatile sig_atomic_t count_decision=1;
+static volatile sig_atomic_t count_log=1;
+static volatile sig_atomic_t count_light=1;
 pthread_cond_t hcond;
 pthread_cond_t hdcond;
+pthread_cond_t logcond;
+pthread_cond_t lightcond;
 
 #define WAIT_TIME_SECONDS 1
 
@@ -85,8 +93,8 @@ void log_ls_id(uint8_t part_no, uint8_t rev, message_t* log_id)
 void *temperature(void *threadp){
 	printf("got in temp thread\n");
 	while(1){
-		//	usleep(100000);
-		pthread_mutex_lock(&htimelock);
+		
+		pthread_mutex_lock(&temp_lock);
 		//printf("temperature thread got mutex\n");
 		if(count_temp != 1){
 
@@ -95,7 +103,7 @@ void *temperature(void *threadp){
 
 			printf("temperature Thread sent signal\n");
 		}
-		pthread_mutex_unlock(&htimelock);
+		pthread_mutex_unlock(&temp_lock);
 	}
 }
 
@@ -143,7 +151,19 @@ void *light_sensor(void *light_sensor_param)
             exit(0);
         }
         
-        printf("the message sent:%s\n", id_message.message); 
+        //printf("the message sent:%s\n", id_message.message); 
+        pthread_mutex_lock(&light_mutex);
+		//printf("decision Thread got mutex\n");
+		
+		if(count_light != 1){
+			
+
+			pthread_cond_signal(&lightcond);
+			count_light = 1;
+			printf("light Thread sent signal\n");	
+
+		}
+		pthread_mutex_unlock(&light_mutex);
     }
      
         
@@ -202,10 +222,7 @@ void *logger(void *logger_param)
     mqd_t logger_queue;
 
     struct mq_attr logger_queue_attr;
-	/*pthread_attr_init(&decision_attr);
-	pthread_attr_setinheritsched(&decision_attr,PTHREAD_EXPLICIT_SCHED);
-	pthread_attr_setschedpolicy(&decision_attr,SCHED_FIFO);
-	temp_params.sched_priority =  rt_max_prio-2;*/
+	
 
     logger_queue_attr.mq_flags=0;
 
@@ -232,7 +249,18 @@ void *logger(void *logger_param)
             exit(0);
         }
         
-        printf("the message received:%s\n", received_log.message); 
+        //printf("the message received:%s\n", received_log.message); 
+
+
+        if(count_log != 1){
+			
+
+			pthread_cond_signal(&logcond);
+			count_log = 1;
+			printf("log Thread sent signal\n");	
+
+		}
+
     }
     pthread_exit(0);
 }
@@ -259,6 +287,10 @@ void main(int argc,char **argv){
 	rc=sched_setscheduler(getpid(),SCHED_FIFO,&main_param);
 	if(rc<0) perror("Main program scheduling:\n");
 */
+    /*pthread_attr_init(&decision_attr);
+	pthread_attr_setinheritsched(&decision_attr,PTHREAD_EXPLICIT_SCHED);
+	pthread_attr_setschedpolicy(&decision_attr,SCHED_FIFO);
+	temp_params.sched_priority =  rt_max_prio-2;*/
 
     sem_init(&temp_sem, 0, 0);
 	if(pthread_create(&light_sensor_td, NULL, light_sensor, NULL)<0)
@@ -281,26 +313,24 @@ void main(int argc,char **argv){
 	while(1){
 		printf("here in main\n");
 
-		pthread_mutex_lock(&htimelock);
+		pthread_mutex_lock(&temp_lock);
 		rc = gettimeofday(&tp,NULL);
 		heartbeat_time.tv_sec = tp.tv_sec;
 		heartbeat_time.tv_nsec = tp.tv_usec * 1000;
 		heartbeat_time.tv_sec +=WAIT_TIME_SECONDS;
 		if(count_temp == 1){
 			count_temp =0;
-			rc=pthread_cond_timedwait(&hdcond,&htimelock,&heartbeat_time);
+			rc=pthread_cond_timedwait(&hdcond,&temp_lock,&heartbeat_time);
 			if(rc==ETIMEDOUT){
 				printf("wait timed out!\n");
-				pthread_mutex_unlock(&htimelock);
+				pthread_mutex_unlock(&temp_lock);
 
 				
 			}
-			pthread_mutex_unlock(&htimelock);
+			pthread_mutex_unlock(&temp_lock);
 			/*conditional wait for temp thread*/
 		}
 		
-
-
 
 		pthread_mutex_lock(&decision_mutex);
 		rc = gettimeofday(&tp,NULL);
@@ -315,6 +345,38 @@ void main(int argc,char **argv){
 				pthread_mutex_unlock(&decision_mutex);
 			}
 			pthread_mutex_unlock(&decision_mutex);
+			/*conditional wait for decision thread*/
+		}
+
+		pthread_mutex_lock(&log_mutex);
+		rc = gettimeofday(&tp,NULL);
+		heartbeat_time.tv_sec = tp.tv_sec;
+		heartbeat_time.tv_nsec = tp.tv_usec * 1000;
+		heartbeat_time.tv_sec +=WAIT_TIME_SECONDS;
+		if(count_log == 1){
+			count_log = 0;
+			rc=pthread_cond_wait(&logcond,&log_mutex);
+			if(rc==ETIMEDOUT){
+				printf("wait timed out!\n");
+				pthread_mutex_unlock(&log_mutex);
+			}
+			pthread_mutex_unlock(&log_mutex);
+			/*conditional wait for decision thread*/
+		}
+
+		pthread_mutex_lock(&light_mutex);
+		rc = gettimeofday(&tp,NULL);
+		heartbeat_time.tv_sec = tp.tv_sec;
+		heartbeat_time.tv_nsec = tp.tv_usec * 1000;
+		heartbeat_time.tv_sec +=WAIT_TIME_SECONDS;
+		if(count_light == 1){
+			count_light = 0;
+			rc=pthread_cond_wait(&lightcond,&light_mutex);
+			if(rc==ETIMEDOUT){
+				printf("wait timed out!\n");
+				pthread_mutex_unlock(&light_mutex);
+			}
+			pthread_mutex_unlock(&light_mutex);
 			/*conditional wait for decision thread*/
 		}
 		
