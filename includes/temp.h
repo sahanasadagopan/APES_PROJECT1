299c338ae0acf075fcd98749845ecec456dda2fc
sahanasadagopan <sahana.sadagopan@gmail.com>
2017-11-01 21:31:35 -0600

    Temp sensor driver completed

--- includes/temp.h
+++ includes/temp.h
@@ -11,6 +11,8 @@
  *                Sahana Sadagopan, CU, ECEE
  */     
 
+#ifndef TEMP_H
+#define TEMP_H
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -32,9 +34,22 @@
 typedef enum {SUCCESS_TEMP,FAILURE_TEMP}temp_ret;
 /*Configuration registure*/
 #define CONF_REG_OS 0X8000
-#define CONF_REG_MAX_RESOLUTION 0X6000
-#define CONF_REG_MAX_S_RESOLUTION 0X4000
-#define CONF_REG_MIN_RESOLUTION 0x0000
-#define CONF_REG_MIN_S_RESOLTUION 0X0X2000
+#define CONF_REG_MAX_RESOLUTION 0X600A
+#define CONF_REG_MAX_S_RESOLUTION 0X400A
+#define CONF_REG_MIN_RESOLUTION 0x000A
+#define CONF_REG_MIN_S_RESOLTUION 0X0200A
+#define CONF_REG_EXTENDED_MODE 0X000B
 
+temp_ret thigh_reg_read(int temp_fd);
+temp_ret thigh_reg_write(int temp_fd,uint16_t value);
+temp_ret tlow_reg_write(int temp_fd,uint16_t value);
+temp_ret tlow_reg_read(int temp_fd);
+temp_ret temp_register(float resolution,int temp_fd);
+temp_ret write_register(int temp_fd,uint8_t pointer_reg_conf,uint16_t* value);
+temp_ret read_register(int temp_fd, uint8_t pointer_reg_conf);
+float set_resolution(char* value,int temp_fd);
+temp_ret pointer_reg(int reg,int temp_fd);
+temp_ret conf_reg_read(int temp_fd);
+void signal_handler(int signum);
 
+#endif
--- src/temp.c
+++ src/temp.c
@@ -15,7 +15,8 @@
 #include "temp.h"
 
 #define DEV_ADDR 0X48
-
+temp_ret read_register(int temp_fd, uint8_t pointer_reg_conf);
+temp_ret write_register(int temp_fd,uint8_t pointer_reg_conf,uint16_t* value);
 /*global variable*/
 
 int file; 
@@ -56,20 +57,30 @@ void signal_handler(int signum)
 
 
 
-temp_ret pointer_reg(int reg){
-
+temp_ret pointer_reg(int reg,int temp_fd){
+	uint8_t *buf = malloc(sizeof(uint8_t)*2);
+	uint8_t buf1 = 0x60;
+	uint8_t buf2 = 0xa0;
+	uint16_t temp;
+	unsigned char MSB,LSB,lsb2;
 	if(reg == 1){
 		uint8_t pointer_reg_conf = POINTER_REG|TEMP_REG_ADDR;
+		//i2c_write(&pointer_reg_conf,temp_fd);
 	}
 	if(reg == 2){
+		
 		uint8_t pointer_reg_conf = POINTER_REG|CONF_REG_ADDR;
+		read_register(temp_fd,pointer_reg_conf);
+		
 	}
 	if(reg == 3){
 		uint8_t pointer_reg_conf = POINTER_REG|TLOW_REG_ADDR;
+		read_register(temp_fd,pointer_reg_conf);
+
 	}
 	if(reg == 4){
-	
-	uint8_t pointer_reg_conf = POINTER_REG|THIGH_REG_ADDR;
+		uint8_t pointer_reg_conf = POINTER_REG|THIGH_REG_ADDR;
+		//i2c_write(&pointer_reg_conf,temp_fd);
 	}
 
 	return SUCCESS_TEMP;
@@ -88,10 +99,10 @@ temp_ret pointer_reg(int reg){
 
 
 
-float set_resolution(char* value){
+float set_resolution(char* value,int temp_fd){
 	float resolution=0;
-	pointer_reg(2);
-	printf("value of input %s\n",value);
+	pointer_reg(2,temp_fd);
+	uint8_t *buf = malloc(sizeof(uint8_t)*2);
 	if(value == "max"){
 		printf("got value\n");
 		uint16_t CONF_REG = CONF_REG_MAX_RESOLUTION;
@@ -107,21 +118,51 @@ float set_resolution(char* value){
 	
 }
 
-void temp_register(float resolution){
+temp_ret read_register(int temp_fd, uint8_t pointer_reg_conf){
+	uint8_t *buf = malloc(sizeof(uint8_t)*2);
+	i2c_write(&pointer_reg_conf,temp_fd);
+	i2c_read_word(buf,temp_fd);
+	printf("resolution set 1st bit%x\n",buf[0]);
+	printf("resol 2nd bit %x\n ", buf[1]);	
+	free(buf);
+
+}
+
+temp_ret write_register(int temp_fd,uint8_t pointer_reg_conf,uint16_t* value){
+
+	uint8_t *buf = malloc(sizeof(uint8_t)*3);
+	//i2c_write(&pointer_reg_conf,temp_fd);
+	//printf("The register written %x \n",pointer_reg_conf);
+	buf[0]=pointer_reg_conf;
+	*((uint16_t*)&buf[1])=*value;
+	i2c_write_word(&buf[0],temp_fd);
+	printf("value to the reg %x \n",value);
+	free(buf);
+
+}
+
+temp_ret conf_reg_read(int temp_fd){
+	uin8_t conf_reg_addr = CONF_REG_ADDDR;
+	read_register(temp_fd,conf_reg_addr);
+}
+
+temp_ret temp_register(float resolution,int temp_fd){
 	unsigned char MSB, LSB;
 	float temp;
 	float f,c;
-	pointer_reg(1);
+	pointer_reg(1,temp_fd);
 	uint8_t *buf = malloc(sizeof(uint8_t)*2);
-	while(1)
+	int count =0;
+	while(count <10)
    	{
-		if((i2c_read(buf))!=SUCCESS){
+		if((i2c_read(buf,temp_fd))!=SUCCESS){
 			perror("Reading error\n");
          	}
 	//printf("buf value %d",buf);
      // Using I2C Read 
 		else {
-			printf("buf value %d",buf);
+			printf("buf value %x",buf[0]);
+			printf("buf value %x",buf[1]);
        			MSB = buf[0];
        			LSB = buf[1];
 
@@ -134,21 +175,54 @@ void temp_register(float resolution){
 
        			printf("Temp Fahrenheit: %f Celsius: %f\n", f, c);
      			}
+		count++;
 	}
 	
 }
+
+temp_ret tlow_reg_read(int temp_fd){
+	uint8_t tlow_reg_addr = TLOW_REG_ADDR;
+	read_register(temp_fd,tlow_reg_addr);
+	
+
+}
+
+temp_ret tlow_reg_write(int temp_fd,uint16_t value){
+	uint8_t tlow_reg_addr = TLOW_REG_ADDR;
+	write_register(temp_fd,tlow_reg_addr,&value);
+
+}
+
+temp_ret thigh_reg_write(int temp_fd,uint16_t value){
+	uint8_t thigh_reg_addr = THIGH_REG_ADDR;
+	write_register(temp_fd,thigh_reg_addr,&value);
+
+}
+
+
+temp_ret thigh_reg_read(int temp_fd){
+	uint8_t thigh_reg_addr = THIGH_REG_ADDR;
+	read_register(temp_fd,thigh_reg_addr);
+}
+
+
+
 int main(){
 
-   
+   int temp_fd;
   char *bus = "/dev/i2c-2"; /* Pins P9_19 and P9_20 */
-  int addr = 0x48;          /* The I2C address of TMP102 */
-  //char buf[2] = {0};
+  uint16_t lvalue = 0x0a;
+  uint16_t hvalue = 0x23;
   float resolution=0.00;
-  i2c_init(DEV_ADDR);
- /* Register the signal handler */
- signal(SIGINT, signal_handler);
- resolution=set_resolution("max");
+  i2c_init(DEV_ADDR,&temp_fd);
+  /*Register the signal handler */
+ resolution=set_resolution("max",temp_fd);
  printf("resolution %f\n", resolution);
- temp_register(resolution);
+ temp_register(resolution,temp_fd);
+ tlow_reg_write(temp_fd,lvalue);
+ thigh_reg_write(temp_fd,hvalue);
+ thigh_reg_read(temp_fd);
+ tlow_reg_read(temp_fd);
+ signal(SIGINT, signal_handler);
  sleep(1);
 }
