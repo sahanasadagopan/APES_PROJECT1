87bdc865259eb960667b0fa977f1799ea961c967
Ashwath Gundepally <ashwath.gundepally@colorado.edu>
2017-11-03 03:16:35 -0600

    added some basic message api stuff- not complete/tested

--- includes/log.h
+++ includes/log.h
@@ -9,6 +9,7 @@
  */
 #ifndef LOG_H
 #define LOG_H
+
 #include <stdint.h>
 
 #define MAX_MSGS_IN_LOG_QUEUE   100
@@ -17,14 +18,55 @@
 
 typedef enum {LOG_INFO, LOG_DRIVER, LOG_ALERT, LOG_CRITICAL} log_level_t;
 
+/* packet that will be written to the logger queue */
+/* this data will be simply dumped to the file */
 typedef struct
 {
-    struct timeval timestamp;    
+    struct timeval timestamp;
     uint16_t length;
     log_level_t log_level;
     char thread_name[30];
     char message[256];
-}message_t;
+}log_message_t;
 
+/* different task names */
+typedef enum { TEMPERATURE_TASK, LIGHT_TASK, MAIN_TASK, LOGGER_TASK, DECISION_TASK } thread_id;
+
+/* the data that can be sent/received by different threads */
+typedef enum { LUMINOSITY_DATA, LIGHT_SENSOR_ID, TEMPERATURE_DATA_C, TEMPERATURE_DATA_F, TEMPERATURE_DATA_K} data_type_t;
+
+/* enum to define whether a message is a request or a response */
+typedef enum { REQUEST_MESSAGE, RESPONSE_MESSAGE} message_type_t;
+
+/* return code for the inter-thread message API*/
+typedef enum { MSGRC_SUCCESS, MSGRC_FAILURE}  message_rc;
+
+
+/* message packet that will be exchanged among threads */
+typedef struct
+{
+    
+    struct timeval timestamp;
+    thread_id tid;
+    message_type_t message_type;
+    data_type_t what_data;
+    void* data;
+
+}thread_message_t;
+
+/* struct to store the light sensor's id in */
+typedef struct
+{
+    /* light sensor's part number */
+    uint8_t part_no;
+    
+    /* light sensor's rev number */
+    uint8_t rev_no;
+}ls_id_val;
+
+
+message_rc request_ls_id(thread_message_t* ls_id_message, ls_id_val* ls_id);
+
+message_rc light_sensor_send_response(thread_message_t* response_message, thread_id tid);
 
 #endif
--- src/log.c
+++ src/log.c
@@ -6,6 +6,7 @@
 #include <sys/time.h>
 #include <mqueue.h>
 #include <semaphore.h>
+#include <inttypes.h>
 
 #include "../includes/light_sensor.h"
 #include "../includes/i2c.h"
@@ -19,6 +20,10 @@ pthread_t light_sensor_td;
 pthread_t decision_thread;
 
 sem_t temp_sem;
+
+
+mqd_t light_sensor_queue;
+
 
 void print_scheduler(void){
 	
@@ -40,85 +45,179 @@ void print_scheduler(void){
 	}
 }
 
-void log_ls_id(uint8_t part_no, uint8_t rev, message_t* log_id)
+message_rc light_sensor_read_queue(thread_message_t* received_message)
+{  
+    
+    if(mq_receive(light_sensor_queue, (char*)received_message, MAX_MSG_SIZE_LOG_QUEUE, NULL)==-1)
+    {   
+        perror("mq_receive failed in read ls queue\n");
+        exit(0);
+    }
+}
+/* identify if the message is a response or a request
+ * and call apt routines*/
+message_rc light_sensor_parse_message(thread_message_t* received_message)
 {
-   // message_t log_id;
-    //struct timeval timestamp;
-    gettimeofday(&log_id->timestamp, NULL);
+    if(received_message->message_type==REQUEST_MESSAGE)
+    {
+        /* a function that sends the apt data that is requested and logs it to the logger queue */
+        
+        return MSGRC_SUCCESS; 
+    }
+    else if(received_message->message_type==RESPONSE_MESSAGE)
+    {
+       /* a function that receives the data and logs to the logger queue */
     
-    strcpy(log_id->thread_name, "light sensor");
-    
-    log_id->log_level=LOG_INFO;
+        return MSGRC_SUCCESS;
+    }
+    else
+        return MSGRC_FAILURE;
+}
 
-    sprintf(log_id->message, "%s: The part number is: %d, and the revision is:%d",\
-            log_id->thread_name, part_no, rev);
+
+/* send a response to the request
+ * message with the apt data to the
+ * queue of the requesting thread.
+ * */
+message_rc light_sensor_create_response(thread_message_t* received_message)
+{
+    /* start creating the packet to be sent */
+    thread_message_t response_message;
     
-    log_id->length=strlen(log_id->message)+sizeof(log_id->log_level)+sizeof(log_id->timestamp) + 1; 
+    /* the message type will be a response */
+    response_message.message_type=RESPONSE_MESSAGE;
+    
+    /* its tid will be LIGHT_TASK */
+    response_message.tid=LIGHT_TASK;
+    
+    /* its data type will be the same as that of the received message */
+    response_message.what_data=received_message->what_data;
+    
+    /* find out which data was requested and then send it by 
+     * calling the apt driver level APIs that interact with
+     * the sensor */
+    switch(response_message.what_data)
+    {
+        case LUMINOSITY_DATA:
+        {   
+            /* create pointer to heap to hold the luminosity value */
+            float* luminosity=(float*)malloc(sizeof(float));
+            
+            /* pass that pointer with the light_sensor's file descriptor and get luminosity */
+            get_luminosity(light_sensor_fd, luminosity);    
+            
+            /* now assign that pointer as the data value of the response packet */
+            response_message.data=(void*)luminosity;
+            
+            /* grab the timestamp now */
+            if(gettimeofday(&(response_message.timestamp), NULL)==-1)
+            {
+                perror("get tod fails in ls send response");
+                exit(0);
+            }
+            /* send it to the requester's queue */
+            light_sensor_send_response(response_message, received_message->tid);
+            
+            break;
+        }
+        case LIGHT_SENSOR_ID:
+        {
+            
+            break;
+        }
+        case LIGHT_SENSOR_IC_REG:
+        {
+            break;
+        }     
+    }
+    
+}
+/* sends packet to the queue of the thread with
+ * the thread id given */
+message_rc light_sensor_send_response(thread_message_t* response_message, thread_id tid)
+{
+    switch(tid)
+    {
+        case LIGHT_TASK:
+            return MSGRC_SUCCESS;
+            break;
+        
+        case TEMPERATURE_TASK:
+            return MSGRC_SUCCESS;
+            break;
+        
+        case MAIN_TASK:
+            return MSGRC_SUCCESS;
+            break;
+        
+        default:
+            return MSGRC_FAILURE;
+            break;    
+    }
+
+    return MSGRC_FAILURE;
 }
 
 
 void *light_sensor(void *light_sensor_param)
 {
-    /* file descriptor */
-    int light_sensor_fd;
+    uint8_t part_no=0, rev=0;
     
-    //i2c_init(LIGHT_SENSOR_DEV_ADDR, &light_sensor_fd);
-    
-    uint8_t part_no=10, rev=5;
-    
-    //read_id_reg(light_sensor_fd, &part_no, &rev);
-    
-    message_t id_message;
-    
-    log_ls_id(part_no, rev, &id_message); 
-    
-    mqd_t logger_queue;
+    thread_message_t received_message;
 
-    struct mq_attr logger_queue_attr;
-
-    logger_queue_attr.mq_flags=0;
-
-    logger_queue_attr.mq_maxmsg=MAX_MSGS_IN_LOG_QUEUE;
-    logger_queue_attr.mq_msgsize=MAX_MSG_SIZE_LOG_QUEUE;
-
-    sem_wait(&temp_sem);
-    logger_queue=mq_open(LOG_QUEUE_NAME, O_RDWR, 666, &logger_queue_attr);
+    light_sensor_read_queue(&received_message); 
+    
+    light_sensor_parse_message(&received_message); 
+     
     
     
-    if(logger_queue==(mqd_t)-1)
+    
+    pthread_exit(0);
+}
+
+void* temperature(void* temperature_sensor_param)
+{
+    ls_id_val id_val;
+    thread_message_t ls_id_message;
+    ls_id_message.tid=TEMPERATURE_TASK;
+
+    request_ls_id(&ls_id_message, &id_val);
+
+    
+}
+
+message_rc request_ls_id(thread_message_t* ls_id_message, ls_id_val* ls_id)
+{
+    ls_id_message->what_data= LIGHT_SENSOR_ID;
+    ls_id_message->message_type=REQUEST_MESSAGE;
+    
+    if(gettimeofday(&(ls_id_message->timestamp), NULL)==-1)
     {
-        perror("mq in light thread open failed");
+        perror("gettimeofday failed in request_ls_id\n");
         exit(0);
     }
-   
     
-   
-    while(1)
+    ls_id_message->data=(void*)ls_id;
+    
+    if(mq_send(light_sensor_queue, (char*)ls_id_message, MAX_MSG_SIZE_LOG_QUEUE, 0)==-1)
     {
-        if(mq_send(logger_queue, (char*)&id_message, MAX_MSG_SIZE_LOG_QUEUE, 0)==-1)
-        {
-            perror("mq_send failed");
-            exit(0);
-        }
-        
-        printf("the message sent:%s\n", id_message.message); 
+        perror("send to ls queue failed\n");
+        exit(0);
     }
-     
-        
-    pthread_exit(0);
+    
 }
 
 void *logger(void *logger_param)
 {
     mqd_t logger_queue;
-
+    
     struct mq_attr logger_queue_attr;
-
+    
     logger_queue_attr.mq_flags=0;
-
+        
     logger_queue_attr.mq_maxmsg=MAX_MSGS_IN_LOG_QUEUE;
     logger_queue_attr.mq_msgsize=MAX_MSG_SIZE_LOG_QUEUE;
-    
+                
     logger_queue=mq_open(LOG_QUEUE_NAME, O_CREAT|O_RDWR, 666, &logger_queue_attr);
     if(logger_queue==(mqd_t)-1)
     {
@@ -127,12 +226,11 @@ void *logger(void *logger_param)
     }
     sem_post(&temp_sem);
     
-    message_t received_log;
-   
-
+    log_message_t received_log;
+    
+    
     while(1)
-    {
-        
+    {    
         if(mq_receive(logger_queue, (char*)&received_log, MAX_MSG_SIZE_LOG_QUEUE, NULL)==-1)
         {
             perror("mq_receive failed");
@@ -146,21 +244,41 @@ void *logger(void *logger_param)
 
 int main(int argc,char **argv)
 {
-    sem_init(&temp_sem, 0, 0);
+    
+    struct mq_attr light_sensor_queue_attr;
+    
+    light_sensor_queue_attr.mq_flags=0;
+        
+    light_sensor_queue_attr.mq_maxmsg=MAX_MSGS_IN_LOG_QUEUE;
+    light_sensor_queue_attr.mq_msgsize=MAX_MSG_SIZE_LOG_QUEUE;
+                
+    light_sensor_queue=mq_open(LOG_QUEUE_NAME, O_CREAT|O_RDWR, 666, &light_sensor_queue_attr);
+    if(light_sensor_queue==(mqd_t)-1)
+    {
+        perror("mq in logger failed");
+        exit(0);
+    }
 	if(pthread_create(&light_sensor_td, NULL, light_sensor, NULL)<0)
     {
         perror("Thread not created\n");
         exit(1);
     }
-
-	if(pthread_create(&logger_td, NULL, logger, NULL)<0)
+    
+/*	if(pthread_create(&logger_td, NULL, logger, NULL)<0)
     {
         perror("Thread not created\n");
 	    exit(1);
     }
-
-    pthread_join(logger_td, NULL);
+*/	
+    if(pthread_create(&temperature_sensor_td, NULL, temperature, NULL)<0)
+    {
+        perror("Thread not created\n");
+	    exit(1);
+    }
+    
+//    pthread_join(logger_td, NULL);
 	pthread_join(light_sensor_td, NULL);
+    pthread_join(temperature_sensor_td, NULL);
     
     return 0;
 }
