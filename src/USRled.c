c30d264911d619b4db002296a7e547ca99f2c61b
sahanasadagopan <sahana.sadagopan@gmail.com>
2017-11-03 13:26:52 -0600

    feature commit:Decision task when temperature is above threshold

---
+++ src/USRled.c
@@ -0,0 +1,20 @@
+#include "../includes/USRled.h"
+
+int detection_led(){
+	FILE* LEDHandle = NULL;
+	const char *LEDBrightness = "/sys/class/leds/beaglebone:green:usr0/brightness";
+int i =0;
+for(i=0;i<2;i++){
+	if((LEDHandle = fopen(LEDBrightness,"r+")) !=NULL){
+		fwrite("1",sizeof(char),1,LEDHandle);
+		fclose(LEDHandle);
+	}
+	usleep(100);
+	if((LEDHandle = fopen(LEDBrightness,"r+")) != NULL){
+		fwrite("0",sizeof(char),1,LEDHandle);
+		fclose(LEDHandle);
+		}
+	//usleep(1000000);
+	}
+	return 0;
+}
